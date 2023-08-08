0bf81748f1629ea7cf8528fe02b84053c16726c7
Aleksandr <titamoto@gmail.com>
2023-08-08 14:31:58 -0700

    complete all methods

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import ForeignKey, Column, Integer, String, MetaData, Boolean, create_engine
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.associationproxy import association_proxy
 
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -45,6 +46,31 @@ class Role(Base):
     character_name = Column(String())
 
     auditions = relationship('Audition', back_populates='role')
+    actors = association_proxy('auditions', 'actor', creator=lambda actor : Audition(actor=actor))
+    locations = association_proxy('auditions', 'location', creator=lambda location : Audition(location=location))
+
+    def lead(self):
+        engine = create_engine('sqlite:///theater.db')
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        hired = session.query(Audition).filter(Audition.role_id == self.id, Audition.hired == True).first()
+        if hired == None:
+            return 'no actor has been hired for this role'
+        return hired
+
+    def understudy(self):
+        engine = create_engine('sqlite:///theater.db')
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        all_hired = session.query(Audition).filter(Audition.role_id == self.id, Audition.hired == True).all()
+        if len(all_hired) < 2:
+            return 'no actor has been hired for understudy for this role'
+        return all_hired[1]
+
 
     def __repr__(self):
         return f'Role(id={self.id}, ' + \
